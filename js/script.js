22f157d747b502564feacde96be4232893d439a7
Joao Correa <joaocorrea.dev@gmail.com>
2023-05-16 18:55:46 -0300

    dropdownmenu to class

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@ import TabNav from './modules/tabnav.js';
 import Modal from './modules/modal.js';
 import Tooltip from './modules/tooltip.js';
 import ScrollAnima from './modules/scroll-anima.js';
+import DropdownMenu from './modules/dropdown-menu.js';
 import fetchAnimais from './modules/fetch-animais.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
-import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 
@@ -28,7 +28,9 @@ tooltip.init();
 const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
 scrollAnima.init();
 
-initDropdownMenu();
+const dropdownMenu = new DropdownMenu('[data-anime="scroll"]');
+dropdownMenu.init('[data-dropdown]');
+
 initMenuMobile();
 initFuncionamento();
 
