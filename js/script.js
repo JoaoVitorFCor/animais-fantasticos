b0afe03a4fb580ba8b9f17c4038739f110861b73
Joao Correa <joaocorrea.dev@gmail.com>
2023-05-22 13:25:47 -0300

    funcionamento to class/ dpmenu back to init

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@ import ScrollAnima from './modules/scroll-anima.js';
 import fetchAnimais from './modules/fetch-animais.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
 import MenuMobile from './modules/menu-mobile.js';
-import DropdownMenu from './modules/dropdown-menu.js';
-import initFuncionamento from './modules/funcionamento.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
+import Funcionamento from './modules/funcionamento.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -31,12 +31,11 @@ scrollAnima.init();
 const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
 menuMobile.init();
 
-const dropdownMenu = new DropdownMenu('[data-menu="button"]', '[data-menu="list"]');
-dropdownMenu.init();
-
-
-initFuncionamento();
+const funcionamento = new Funcionamento('[data-semana]', 'aberto');
+funcionamento.init();
 
 fetchAnimais('../../animaisapi.json', '.numeros-grid');
 
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
+
+initDropdownMenu();
